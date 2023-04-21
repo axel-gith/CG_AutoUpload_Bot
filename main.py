7c4fc61f906b30d3aa625906f4c9b0d659bc89b8
Axel Antoci <axel.antoci@cyberguru.eu>
2023-04-21 16:59:09 +0200

    beginning of code organization

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import os
 import csv
+import sys
+
 import my_variables
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -13,22 +15,44 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 
+
+# ============================================================================================ FUNCTIONS ========
+def getModuleStrings(module, Path):
+
+    return
+
+# ============================================================================================ GLOBAL VARIABLES ========
+options = Options()
+# options.add_argument('--headless') #Hide GUI
+options.add_argument("--windows-size=1920, 1080")
+options.add_argument("start-maximized")
+options.add_experimental_option("prefs", {"profile.managed_default_content_setting.images": 2})
+rawPath = os.getcwd()
+Path = rawPath.replace("\\", "/") + "/"
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("0) Dev\n1) Enterprise\n2) Enterprise-ww\n3) Pirelli\n")
-    choice = int(input("Choose an instance: "))
+    modified_quiz_count = 0
+    # ====================================================================================================== MENU ======
+    print("0) Dev\n1) Enterprise\n2) Enterprise-ww\n3) Pirelli\n4) awareness\n5) international")
+    choice = int(input("\nChoose an instance: "))
+    print("\n")
     questions_answers_to_upload = []
-
     instance = ""
     match choice:
         case 0:
-            instance = "dev.enterprise"
+            instance = "dev.enterprise.cyberguru.it"
         case 1:
-            instance = "enterprise"
+            instance = "enterprise.cyberguru.it"
         case 2:
-            instance = "enterprise-ww"
+            instance = "enterprise-ww.cyberguru.it"
         case 3:
-            instance = "pirelli"
+            instance = "pirelli.cyberguru.it"
+        case 4:
+            instance = "awareness.cyberguru.it"
+        case 5:
+            instance = "international.cyberguru.it"
         case _:
             print("No instance selected....Closing application")
     if instance:
@@ -49,17 +73,10 @@ if __name__ == '__main__':
 
         module_number = questions_answers_to_upload[0][0].split("Q", 1)[0]
 
-        options = Options()
-        # options.add_argument('--headless') #Hide GUI
-        options.add_argument("--windows-size=1920, 1080")
-        options.add_argument("start-maximized")
-        options.add_experimental_option("prefs", {"profile.managed_default_content_setting.images": 2})
-        #chrome_options = webdriver.ChromeOptions()
-        #chrome_options.add_experimental_option("prefs", {"profile.managed_default_content_setting.images": 2}) #no download images
 
-        driver = webdriver.Chrome(options=options)
         # ================================================================================================= LOGIN ======
-        url = "https://" + instance + ".cyberguru.it"
+        driver = webdriver.Chrome(options=options)
+        url = "https://" + instance
         driver.get(url)
         element = WebDriverWait(driver=driver, timeout=20).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "#username")))
@@ -115,6 +132,7 @@ if __name__ == '__main__':
                     EC.presence_of_element_located((By.CSS_SELECTOR, "#id_generalheader")))
                 for test in questions_answers_to_upload:
                     if test[0] == driver.find_element(By.ID, "id_name").get_attribute('value'):
+                        driver.implicitly_wait(2)
                         question_textarea = driver.find_element(By.ID, "id_questiontexteditable")
                         answer0_textarea = driver.find_element(By.ID, "id_answer_0editable")
                         answer1_textarea = driver.find_element(By.ID, "id_answer_1editable")
@@ -122,16 +140,27 @@ if __name__ == '__main__':
 
                         driver.execute_script("arguments[0].innerHTML = arguments[1];", question_textarea, test[1])
                         driver.execute_script("arguments[0].focus()", question_textarea)
+                        driver.implicitly_wait(0.2)
                         driver.execute_script('arguments[0].innerHTML = arguments[1];', answer0_textarea, test[2])
                         driver.execute_script("arguments[0].focus()", answer0_textarea)
+                        driver.implicitly_wait(0.2)
                         driver.execute_script('arguments[0].innerHTML = arguments[1];', answer1_textarea, test[3])
                         driver.execute_script("arguments[0].focus()", answer1_textarea)
+                        driver.implicitly_wait(0.2)
                         driver.execute_script('arguments[0].innerHTML = arguments[1];', answer2_textarea, test[4])
                         driver.execute_script("arguments[0].focus()", answer2_textarea)
-                        #print("changed text, saving")
+                        driver.implicitly_wait(0.2)
+                        print(test[0] + " finished")
+
+                        modified_quiz_count += 1
+
                         driver.find_element(By.ID, "id_submitbutton").click()
-
-
+                        element = WebDriverWait(driver=driver, timeout=20).until(
+                            EC.presence_of_element_located((By.CSS_SELECTOR, "#page-1")))
+                        if modified_quiz_count == len(questions_answers_to_upload):
+                            print("Modified all quizzes present in file, closing...")
+                            driver.quit()
+                            sys.exit()
                         # driver.find_element(By.ID, "id_cancel").click()
                         break
         print("finished editing " + module_number + " on instance " + instance)
